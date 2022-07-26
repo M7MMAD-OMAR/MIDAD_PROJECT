ac662a8da9645596d34ea7390674d6a826923bb4
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-07-27 01:08:46 +0300

    create post.blade.php and change posts.blade.php

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>posts</title>
+    <style>
+        body {
+            background-color: #2d3748;
+            color: #f7fafc;
+            display: flex;
+            justify-content: center;
+            align-content: center;
+        }
+        body div {
+            max-width: 50%;
+        }
+    </style>
+</head>
+<body>
+
+<div>
+    <artisan>
+        <a href="{{}}"><h1>First post</h1></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, ipsum!</p>
+    </artisan>
+    <artisan>
+        <a href=""><h1>Second post</h1></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, minus.</p>
+    </artisan>
+    <artisan>
+        <a href=""><h1>Third post</h1></a>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, itaque?</p>
+    </artisan>
+</div>
+</body>
+</html>
