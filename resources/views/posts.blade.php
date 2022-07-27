e7e5874c415c9d8426101103dd63a2ae370e12bb
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-07-27 18:15:15 +0300

    clear code

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -26,16 +26,19 @@
 </head>
 <body>
 
-    <div>
-        <artisan>
-            @foreach($posts as $post)
-                <h1><a href="/posts/{{$post->slug}}">{{$post->title}}</a></h1>
-                <div>
-                    {{$post->excerpt}}
-                </div>
-
-        @endforeach
-    </div>
+<div>
+    <artisan>
+        @foreach($posts as $post)
+            <h1>
+                <a href="/posts/{{$post->slug}}">
+                    {{$post->title}}
+                </a>
+            </h1>
+            <div>
+                {{$post->excerpt}}
+            </div>
+    @endforeach
+</div>
 
 </body>
 </html>
